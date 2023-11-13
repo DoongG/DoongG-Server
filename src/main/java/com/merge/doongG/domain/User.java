353f90e01239c8523a8ccf4e8142439ea3cf7f51
eukkki210 <ysg980423@gmail.com>
2023-11-13 17:43:27 +0900

    Feat: 스프링 swagger, jpa, h2 기본 환경 구축

--- src/main/java/com/merge/doongG/domain/User.java
+++ src/main/java/com/merge/doongG/domain/User.java
@@ -1,10 +1,14 @@
 package com.merge.doongG.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
+@Table(name = "members")
 @Getter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
 
 }
