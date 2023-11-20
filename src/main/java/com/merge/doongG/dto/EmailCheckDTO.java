e72b31cd5eed06a5d3cc6d70507fbdc610acdc44
jaejae990921 <jaejae990921@naver.com>
2023-11-20 17:33:44 +0900

    Feat: 이메일 및 닉네임 중복체크, 문자인증, 회원가입, 로그인 기능 구현

---
+++ src/main/java/com/merge/doongG/dto/EmailCheckDTO.java
@@ -0,0 +1,9 @@
+package com.merge.doongG.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class EmailCheckDTO {
+    String email;
+}
--- src/main/java/com/merge/doongG/repository/UserRepository.java
+++ src/main/java/com/merge/doongG/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email); // 이메일로 유저 찾기
     Optional<User> findByNickname(String nickname); // 닉네임으로 유저 찾기
+    Optional<User> findByPhoneNumber(String phoneNumber); // 전화번호로 유저 찾기
 }
