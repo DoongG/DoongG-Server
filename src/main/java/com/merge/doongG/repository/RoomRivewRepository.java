cd6b068732aba1b3667e56bdd2ca9de2bd07e474
jaejae990921 <jaejae990921@naver.com>
2023-11-29 20:58:15 +0900

    Feat: 자취방 리뷰 x,y 값 통일 시키는 기능 추가
    - 같은 주소로 자취방 리뷰 작성 시, 마커 경도,위도 통일

--- src/main/java/com/merge/doongG/repository/RoomRivewRepository.java
+++ src/main/java/com/merge/doongG/repository/RoomRivewRepository.java
@@ -3,12 +3,17 @@ package com.merge.doongG.repository;
 import com.merge.doongG.domain.RoomRivew;
 import com.merge.doongG.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoomRivewRepository extends JpaRepository<RoomRivew, Long> {
     // user가 작성한 리뷰 조회
     List<RoomRivew> findByUser(User user);
+
+    // 주소를 기준으로 리뷰 조회
+    List<RoomRivew> findByAddress(String address);
 }
