7351c398b80e63ebf552456d028c92a0cf49f45e
eukkki210 <ysg980423@gmail.com>
2023-12-07 15:38:24 +0900

    Feat: 내가 쓴 자취방 리뷰 삭제, carousel 에러 픽스

--- src/main/java/com/merge/doongG/controller/RoomRivewController.java
+++ src/main/java/com/merge/doongG/controller/RoomRivewController.java
@@ -5,12 +5,10 @@ import com.merge.doongG.dto.MyRoomRivewDTO;
 import com.merge.doongG.dto.WriteRoomRivewDTO;
 import com.merge.doongG.service.RoomRivewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -41,5 +39,18 @@ public class RoomRivewController {
         UUID uuid = UUID.fromString((String) SecurityContextHolder.getContext().getAuthentication().getDetails());
         List<MyRoomRivewDTO> result = roomRivewService.getMyRoomRivew(uuid);
         return ResponseEntity.ok().body(result);
+    }
+
+    // 내가 쓴 자취방 리뷰 삭제
+    @PostMapping("/roomRivewDelete/{reviewId}")
+    public ResponseEntity<String> deleteRoomRivew(@PathVariable Long reviewId) {
+        UUID uuid = UUID.fromString((String) SecurityContextHolder.getContext().getAuthentication().getDetails());
+        try {
+            roomRivewService.deleteRoomRivew(uuid, reviewId);
+            return ResponseEntity.ok().body("true");
+        } catch (RuntimeException e) {
+            // 해당 리뷰를 찾을 수 없을 때 핸들링
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("false");
+        }
     }
 }
