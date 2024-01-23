4c637ed3f098e6ed3e3f3926d73b6f4e99dec94a
eukkki210 <ysg980423@gmail.com>
2024-01-23 19:45:53 +0900

    Refactor: Production용 수정

--- src/main/java/com/merge/doongG/DoongGApplication.java
+++ src/main/java/com/merge/doongG/DoongGApplication.java
@@ -1,8 +1,15 @@
 package com.merge.doongG;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DoongGApplication {
@@ -11,4 +18,39 @@ public class DoongGApplication {
 		SpringApplication.run(DoongGApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// SSL 트래픽 사용
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// HTTPS 리디렉션에 HTTP 추가
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+		return tomcat;
+	}
+
+	/*
+     HTTP에서 HTTPS로 리디렉션
+     SSL이 없는 경우 이 애플리케이션은 포트 8082를 사용
+     SSL을 사용하면 포트 8443을 사용
+     따라서 8082에 대한 모든 요청은 8443에서 HTTPS로 리디렉션
+      */
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
+		return connector;
+	}
 }
